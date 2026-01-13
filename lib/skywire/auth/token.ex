05bec060c92dff0438684ba7acf15c42e09eacbe
Matthew Terenzio <mterenzio@gmail.com>
2026-01-12 19:42:40 -0500

    fix db uuid

--- lib/skywire/auth/token.ex
+++ lib/skywire/auth/token.ex
@@ -5,6 +5,8 @@ defmodule Skywire.Auth.Token do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "api_tokens" do
     field :name, :string
     field :token_hash, :string
