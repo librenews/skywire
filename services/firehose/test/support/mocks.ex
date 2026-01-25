0ebdc5c10e73bd87cebe72ea4d827bc1969d350e
Matthew Terenzio <mterenzio@gmail.com>
2026-01-25 11:13:41 -0500

    Fix Tracks#feed error and filter reposts in Firehose Matcher

--- services/firehose/test/support/mocks.ex
+++ services/firehose/test/support/mocks.ex
@@ -1,4 +1,4 @@
-Mox.defmock(Skywire.ML.CloudflareMock, for: Skywire.ML.Cloudflare)
+
 Mox.defmock(Skywire.Search.OpenSearchMock, for: Skywire.Search.OpenSearch)
 Mox.defmock(Skywire.RedisMock, for: Skywire.Redis)
 
